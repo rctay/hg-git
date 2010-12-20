de37b53b296b6d572bafed8447dfbe3b78fb54ef
Tay Ray Chuan <rctay89@gmail.com>
2010-12-20 12:17:56 +0800

    pass hg's ui.ssh config to dulwich
    This allows Windows users to override dulwich's default (the unix-y
    ssh).

---
+++ hggit/ssh.py
@@ -0,0 +1,26 @@
+"""Parent class for ui-linked Vendor classes."""
+class SSHVendor(object):
+    pass
+
+
+"""
+Allows dulwich to use hg's ui.ssh config. The dulwich.client.get_ssh_vendor
+property should point to the return value.
+"""
+def generate_ssh_vendor(ui):
+
+    class _Vendor(SSHVendor):
+        def connect_ssh(self, host, command, username=None, port=None):
+            from dulwich.client import SubprocessWrapper
+            from mercurial import util
+            import subprocess
+
+            sshcmd = ui.config("ui", "ssh", "ssh")
+            args = util.sshargs(sshcmd, host, username, port)
+
+            proc = subprocess.Popen([sshcmd, args] + command,
+                                    stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE)
+            return SubprocessWrapper(proc)
+
+    return _Vendor
